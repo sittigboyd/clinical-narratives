5feb757378b262d85d97cb02e8228fdcd8b1ff59
Katie Sittig-Boyd <loverofcapslock@gmail.com>
2015-10-24 18:10:19 -0400

    updated medication, family, history, diabetes functions

--- patient.py
+++ patient.py
@@ -1,21 +1,46 @@
 class Patient(self):
-   def __init__(self,n=None,a=None,d_status=None,idno=None):
-       self.name=n
-       self.age=a
-       self.patient_id=idno
-       self.diabetic=d_status # boolean 
-       self.medications=[]
-       self.history=[] # a blank slate 
-       self.smoking=[]
-       #TODO make sure that medications, history, patient info are all sets of tuples (date, info)
+    def __init__(self,n=None,a=None,d_status=None,idno=None):
+        self.name=n
+        self.age=a
+        self.patient_id=idno
+        self.diabetes=Set()
+        self.medications=Set()
+        self.history=Set()
+        self.smoking=Set()
            
-   def add_medication(self,m):
-       self.medications.append(m)
+    def add_medication(self,date,med):
+        med_add=(date,med)
+        self.medications.add(med_add)
+        
+    def add_smoking(self,date,sm):
+        smoke=(date,sm)
+        self.smoking.add(smoke)
+        
+    def add_history(self,date,h):
+        hist=(date,h)
+        self.history.add(hist)
+        
+    def add_diabetes(self,date,dia):
+        diab=(date,dia)
+        self.diabetes.add(diab)
+        
+        
+    
        
-   def remove_medication(self,m):
-       self.medications.remove(m)
+   
        
-       
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
        
        
        
